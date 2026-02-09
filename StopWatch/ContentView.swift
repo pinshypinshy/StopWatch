35564598377470e437e19540a0d461d3647f20d8
pinshypinshy <hrisyklit@gmail.com>
2026-02-09 17:58:50 +0900

    stopwatch

--- StopWatch/ContentView.swift
+++ StopWatch/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var timer: Timer!
+    @State private var secondsElapsed: Double = 0.0
+    @State private var isRunning: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(String(format: "%.2f", secondsElapsed))
+                .font(.title)
+            HStack {
+                if isRunning {
+                    Button {
+                        pause()
+                    } label: {
+                        Image(systemName: "pause.fill")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .padding()
+                            .background(.orange)
+                            .clipShape(.circle)
+                    }
+                } else {
+                    Button {
+                        start()
+                    } label: {
+                        Image(systemName: "play.fill")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .padding()
+                            .background(.green)
+                            .clipShape(.circle)
+                    }
+                }
+                if secondsElapsed != 0.0 {
+                    Button {
+                        stop()
+                    } label: {
+                        Image(systemName: "stop.fill")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .padding()
+                            .background(.red)
+                            .clipShape(.circle)
+                    }
+                }
+            }
         }
         .padding()
+    }
+    
+    func start() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            secondsElapsed += 0.1
+        }
+        isRunning = true
+    }
+    
+    func pause() {
+        timer.invalidate()
+        isRunning = false
+    }
+    
+    func stop() {
+        timer.invalidate()
+        isRunning = false
+        secondsElapsed = 0.0
     }
 }
 
