5f4e58421dea293491b0a1ed411fd0c6228a1381
pinshypinshy <hrisyklit@gmail.com>
2026-02-09 17:18:07 +0900

    Initial Commit

---
+++ StopWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StopWatch
+//
+//  Created by 平石悠生 on 2026/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by 平石悠生 on 2026/02/09.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
